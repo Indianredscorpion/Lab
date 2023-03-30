6a2e3c73c934bfe23db15dbe66c20844a2aee4b9
OMKAR NASHTE <49943470+Indianredscorpion@users.noreply.github.com>
2023-03-30 18:32:11 +0530

    Create Half_Pyramid.java

---
+++ Lab-30-03-2023/Half_Pyramid.java
@@ -0,0 +1,18 @@
+public class Half_Pyramid {
+    	public static void main(String[] args) {
+      //loop for row
+        
+       for(int i=1;i<=3;i++) { 
+         //column loop
+		    	   for(int j =0;j<i;j++) {
+		    		   System.out.print("*");
+		    	   }
+		    	   System.out.println();
+		       }
+      
+      
+      }
+
+
+
+}
