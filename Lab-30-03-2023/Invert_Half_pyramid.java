947457717e908ba7353d8b35caca5c3c0535930a
OMKAR NASHTE <49943470+Indianredscorpion@users.noreply.github.com>
2023-03-30 18:35:13 +0530

    Create Invert_Half_pyramid.java

---
+++ Lab-30-03-2023/Invert_Half_pyramid.java
@@ -0,0 +1,14 @@
+public class Invert_Half_pyramid {
+public static void main(String[] args){
+for(int i=0;i<=3;i++) { //row
+		    	   for(int j =i;j<3;j++) {//column
+		    		   System.out.print("*");
+		    	   }
+		    	   System.out.println();
+		       }
+
+
+}
+
+
+}
