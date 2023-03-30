e89ea8feb02aae5982e8a5ae8858718524476dd4
OMKAR NASHTE <49943470+Indianredscorpion@users.noreply.github.com>
2023-03-30 18:37:38 +0530

    Create FactorialGivenNumber.java

---
+++ Lab-30-03-2023/FactorialGivenNumber.java
@@ -0,0 +1,13 @@
+public class FactorialGivenNumbe{
+public static void main (String[] args){
+for(int j=1;j<=10;j++) {
+		    	   int fac =1;
+		       for(int i=1;i<=j;i++) { //row
+		    	   fac=fac*i;
+		    	  
+		       }
+		       System.out.println(fac);
+		       }
+
+}
+}
